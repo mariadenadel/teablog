fead22b23208d30622c03bf33d7d7e2ac30ec7a1
MariiaDenysova <maria.denadel@gmail.com>
2021-04-01 15:30:38 +0200

    Create new module "Article"

--- teanews/models.py
+++ teanews/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=150)
+    preview = models.CharField(max_length=250)
+    full_text = models.TextField()
+    created_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новина'
+        verbose_name_plural = 'Новини'
--- teanews/views.py
+++ teanews/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 
 def teanews_home(request):
-    return render(request, 'teanews/teanews_home.html')
+    news = Article.objects.order_by('-created_at')
+    return render(request, 'teanews/teanews_home.html', {'news':news})
